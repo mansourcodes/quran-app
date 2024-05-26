7bd9799948d1105c8ad5af22e592e25066d29cef
Mansour Alnasser <alnassre.it@gmail.com>
2024-05-26 21:30:43 +0300

    setup tailwind

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -4,8 +4,18 @@ import './ExploreContainer.css';
 const ExploreContainer: React.FC = () => {
   return (
     <div id="container">
+      <h1 className="text-3xl font-bold underline bg-red-800">Hello world!</h1>
       <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p>
+        Start with Ionic{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://ionicframework.com/docs/components"
+        >
+          UI Components
+        </a>
+      </p>
     </div>
   );
 };
